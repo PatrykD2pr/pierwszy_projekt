aca9491cce3b5af6626ff65175b498eaddc3f9af
PatrykD2pr <patrykdomaszek@gazeta.pl>
2022-04-07 13:00:37 +0200

    finished

--- app/src/main/java/com/example/pierwszy_projekt/MainActivity.kt
+++ app/src/main/java/com/example/pierwszy_projekt/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.pierwszy_projekt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var next = findViewById<Button>(R.id.next)
+        var prev = findViewById<Button>(R.id.prev)
+        var img = findViewById<ImageView>(R.id.image)
+        var zdj = 1
+
+        img.setImageResource(R.drawable.apex_legends_logo_opt_jpg)
+        zdj = 1
+
+        next.setOnClickListener()
+        {
+            if (zdj % 2 != 0)
+            {
+                img.setImageResource(R.drawable.logolol)
+                zdj++
+            }
+            else
+            {
+                img.setImageResource(R.drawable.apex_legends_logo_opt_jpg)
+                zdj++
+            }
+        }
+
+        prev.setOnClickListener()
+        {
+            if (zdj % 2 != 0)
+            {
+                img.setImageResource(R.drawable.logolol)
+                zdj--
+            }
+            else
+            {
+                img.setImageResource(R.drawable.apex_legends_logo_opt_jpg)
+                zdj--
+            }
+        }
     }
 }
